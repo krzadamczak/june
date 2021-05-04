fd46b9e3037c11972db68c0b5c027a5af3ec890b
Krzysztof Adamczak <krzadamczak@gmail.com>
2021-05-04 23:01:30 +0200

    Dodałem CSS + testy

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+
+wp_enqueue_style('style123', get_theme_file_uri('style.css'));
+
+?>
--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<body>
 <?php 
 
 get_header();
 
-get_template_part('template-parts/site-header');
+?>
+<body class="page-wrapper">
+<?php 
 
+get_template_part('template-parts/site-header');
+    echo get_theme_file_uri('\test/style.css');
+    echo nl2br("\n\n");
+    echo get_theme_file_path('\test/style.css');
 ?>
 </body>
 </html>
