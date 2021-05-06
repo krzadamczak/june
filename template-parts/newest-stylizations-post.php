0959ab66349aedd3ef22e148ff6df7919808c4db
Krzysztof Adamczak <krzadamczak@gmail.com>
2021-05-06 22:33:35 +0200

    Testy efektów przy najechaniu myszką na post

--- template-parts/newest-stylizations-post.php
+++ template-parts/newest-stylizations-post.php
@@ -1,3 +1,4 @@
-<div class="newest-stylizations__post">
-    <img class="newest-stylizations__img" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" srcset="">
+<div class="post">
+    <img class="post__img" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" srcset="">
+    <!-- <div class="post__title"><?php echo the_title(); ?></div> -->
 </div>
