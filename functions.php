7f8f34f1a117f668a0a5218930725d1d9d3b8c31
Krzysztof Adamczak <krzadamczak@gmail.com>
2021-05-13 06:21:34 +0200

    Zmieniłem długość podglądu opisu w sekcji Ostatnie artykuły + drobne poprawki w wyglądzie

--- functions.php
+++ functions.php
@@ -4,4 +4,10 @@ wp_enqueue_style('style123', get_theme_file_uri('style.css'));
 
 add_theme_support('post-thumbnails');
 
+function custom_excerpt_length(){
+    return 40;
+}
+
+add_filter('excerpt_length', 'custom_excerpt_length');
+
 ?>
